9c5d26889696b43f0b4a76c6b8d0dcabc42feb11
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2023-05-31 09:05:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// start logic
+
+let menu = document.querySelector('#menu-btn');
+let navbarLinks = document.querySelector('.header .navbar .links');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbarLinks.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbarLinks.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .navbar').classList.add('active');
+   }else{
+      document.querySelector('.header .navbar').classList.remove('active');
+   }
+}
